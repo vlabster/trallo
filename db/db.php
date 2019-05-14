4a79d5b304303462ac4bfbf03faeb57462012caf
vlabster <vlabster@mail.ru>
2019-05-14 21:38:24 +0300

    add database connection, database config

---
+++ db/db.php
@@ -0,0 +1,7 @@
+<?php
+    require_once('config.php');
+    $mysqli = new mysqli ($host. $user, $password, $database);
+    if ($mysqli->connect_errno) {
+        echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+?>
