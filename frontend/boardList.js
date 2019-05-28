d1543334aa768cd68dc13f83d82f86a6c93197b5
vlabster <vlabster@mail.ru>
2019-05-29 01:26:26 +0300

    boards view

--- frontend/boardList.js
+++ frontend/boardList.js
@@ -1,6 +1,12 @@
-webix.ui ({
-    rows: [
-        { template: "qqw"},
-        { template: "wwq"},
-    ]
+webix.ui({
+    view:"dataview",
+  	width:1000,
+  	height:500,
+    //css: "movies",
+    // type:{
+    //   width: 600,
+    //   height: 200,
+    //   template:"html->template_container"
+    // },
+    data: getData
 });
