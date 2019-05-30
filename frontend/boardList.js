954c6201989cbfdf4db6b9b0683877f62ee55f0a
vlabster <vlabster@mail.ru>
2019-05-30 21:57:57 +0300

    add redirect by click in board

--- frontend/boardList.js
+++ frontend/boardList.js
@@ -1,3 +1,11 @@
+function handler(id) {
+  webix.message(id);
+  //webix.message(document.location.search);
+  let url = document.location.search;
+  location.href = `http://trallo/cards.php${url}&board=${id}`;
+  //webix.message(`http://trallo/cards.php${url}&board=${id}`);
+}
+
 webix.ui({
     container: "areaB",
     view:"dataview",
@@ -5,9 +13,10 @@ webix.ui({
   	height:500,
     css: "movies",
     type:{
-      width: 600,
-      height: 200,
+      width: 260,
+      height: 90,
       template:"html->template_container"
     },
+    click: handler,
     data: getData
 });
